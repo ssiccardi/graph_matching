529723598cc88c30e50239c4abdfd5200c7132c6
roccobalocco <pietro.masolini@studenti.unimi.it>
2022-12-17 02:31:52 +0100

    Test e modifiche a RelationMatching

--- Functions/RelationMatching.py
+++ Functions/RelationMatching.py
@@ -1,4 +1,4 @@
-
+import pandas as pd
 from Functions.AlgorithmUtilities import createTypeBucket, getRel, getSemi, overLimit, sameRel, sameSource, sameTarget
 from Functions.Relation import areDirectlyContraddictory, isFissa
 from Functions.Connection import Connection
@@ -7,7 +7,7 @@ from Functions.Connection import Connection
 ##TODO: Controllo relazioni scadenza funziona?
 ##TODO: Amplia gli attributi che prendi con le get e togli confronti evitabili tramite interrogazione DB, tipo sameTarget
 
-def getInfoSemi(id: int, conn: Connection):
+def getInfoSemi(id: int, conn: Connection, df: pd.DataFrame) -> pd.DataFrame:
     """Analizza le relazioni SemiFisse legate all'id passato in input, mostrando all'utente cosa nota
 
     Args:
@@ -24,25 +24,25 @@ def getInfoSemi(id: int, conn: Connection):
     for r1 in relE1:
         for r2 in relE2:
             if areDirectlyContraddictory(r1.get("tipo"), r2.get("tipo"), r1.get("id"), r2.get("id"), conn):
-                print("Contraddittoria DIRETTA [{}] - [{}]".format(r1, r2))  # CONTRADDITTORIA
+                print("Contraddittoria DIRETTA [{}] - [{}] -- getInfoSemi->areDirectlyContraddictoryEntranti".format(r1, r2))  # CONTRADDITTORIA
                 toRem.append(r2.get("id"))
                 # b = False
                 # break #ora tolti
             if sameRel(r1.get("id"), r2.get("id"), id, 1, conn) and sameSource(r1.get("id"), r2.get("id"), conn):  # capisci se si può fare lo stesso usando le info disponibili in relE1 e relE2 (tipo e1.get('da') == ...)
-                print("Relazione coincidente in G1 di tipo {} con entità di partenza id:{} e entità di arrivo id:{}".format(r1.get("tipo"), r1.get("da"), r1.get("a")))
+                print("Relazione coincidente in G1 di tipo {} con entità di partenza id:{} e entità di arrivo id:{} -- getInfoSemi->sameRelEntranti".format(r1.get("tipo"), r1.get("da"), r1.get("a")))
                 typeBucket[r1.get("tipo")] += 1
                 b = False
                 toRem.append(r2.get("id"))
                 break
         if b:
             # COMPLEMENTARI
-            print("Relazione complementare in G1 di tipo {} con entità di partenza id:{} e entità di arrivo id:{}".format(r1.get("tipo"), r1.get("da"), r1.get("a")))  
+            print("Relazione complementare in G1 di tipo {} con entità di partenza id:{} e entità di arrivo id:{} -- getInfoSemi->if(b)Entranti".format(r1.get("tipo"), r1.get("da"), r1.get("a")))  
             typeBucket[r1.get("tipo")] += 1
         b = True
     for el in relE2:  
         if not len(toRem):
             typeBucket[el.get("tipo")] += 1
-            print("Relazione complementare in G2 di tipo {} con entità di partenza id:{} e entità di arrivo id:{}".format(el.get("tipo"), el.get("da"), el.get("a")))  
+            print("Relazione complementare in G2 di tipo {} con entità di partenza id:{} e entità di arrivo id:{} -- getInfoSemi->penultimoForEntranti".format(el.get("tipo"), el.get("da"), el.get("a")))  
         else:
             b = False
             for id in toRem:
@@ -50,10 +50,10 @@ def getInfoSemi(id: int, conn: Connection):
                     b = True
             if not b:
                 typeBucket[el.get("tipo")] += 1
-                print("Relazione complementare in G2 di tipo {} con entità di partenza id:{} e entità di arrivo id:{}".format(el.get("tipo"), el.get("da"), el.get("a"))) 
+                print("Relazione complementare in G2 di tipo {} con entità di partenza id:{} e entità di arrivo id:{} -- getInfoSemi->penultimoForEntranti".format(el.get("tipo"), el.get("da"), el.get("a"))) 
     
     for t in typeBucket.keys():
-        print("Relazione semifissa {} -- {}".format(t, overLimit(t, typeBucket.get(t), relE1, relE2, conn)))
+        print("Relazione semifissa {} -- {} -- getInfoSemi-> ultimoForEntranti".format(t, overLimit(t, typeBucket.get(t), relE1, relE2, conn)))
 
     # Parte partenti:
     toRem = list()
@@ -61,25 +61,25 @@ def getInfoSemi(id: int, conn: Connection):
     for r1 in relP1:
         for r2 in relP2:
             if sameRel(r1.get("id"), r2.get("id"), id, 0, conn) and sameTarget(r1.get("id"), r2.get("id"), conn):  # capisci se si può fare lo stesso usando le info disponibili in relE1 e relE2 (tipo e1.get('da') == ...)
-                print("Relazione coincidente in G1 di tipo {} con entità di partenza id:{} e entità di arrivo id:{}".format(r1.get("tipo"), r1.get("da"), r1.get("a"))) 
+                print("Relazione coincidente in G1 di tipo {} con entità di partenza id:{} e entità di arrivo id:{} -- getInfoSemi->sameRelPartenti".format(r1.get("tipo"), r1.get("da"), r1.get("a"))) 
                 b = False
                 toRem.append(r2.get("id"))
                 break
         if b:
-            print("Relazione complementare in G1 di tipo {} con entità di partenza id:{} e entità di arrivo id:{}".format(r1.get("tipo"), r1.get("da"), r1.get("a")))  
+            print("Relazione complementare in G1 di tipo {} con entità di partenza id:{} e entità di arrivo id:{} -- getInfoSemi->if(b)Partenti".format(r1.get("tipo"), r1.get("da"), r1.get("a")))  
         b = True
 
     for el in relP2:
         if not len(toRem):
-            print("Relazione complementare in G2 di tipo {} con entità di partenza id:{} e entità di arrivo id:{}".format(el.get("tipo"), el.get("da"), el.get("a")))
+            print("Relazione complementare in G2 di tipo {} con entità di partenza id:{} e entità di arrivo id:{} -- getInfoSemiPartenti ultimo for".format(el.get("tipo"), el.get("da"), el.get("a")))
         else:
             b = False
             for id in toRem:
                 if id == el.get("id"):
                     b = True
             if not b:
-                print("Relazione complementare in G2 di tipo {} con entità di partenza id:{} e entità di arrivo id:{}".format(el.get("tipo"), el.get("da"), el.get("a"))) 
-
+                print("Relazione complementare in G2 di tipo {} con entità di partenza id:{} e entità di arrivo id:{} -- getInfoSemiPartenti ultimo for".format(el.get("tipo"), el.get("da"), el.get("a"))) 
+    return df
 
 def relationMatching(id: int, conn: Connection):
     """Analizza le relazioni legate all'id passato in input, mostrando all'utente cosa nota\n
@@ -91,6 +91,7 @@ def relationMatching(id: int, conn: Connection):
         id (int): [id appartenente all'entita' da analizzare]\n
         conn (Connection): [oggetto dedicato alla connessione a Neo4j]
     """    
+    df = pd.DataFrame(data=None, index=["Rilevazione", "ID Entita' sorgente", "Attributi entita' sorgente", "Grafo sorgente", "ID Entita' destinazione", "Attributi entita' destinazione", "Grafo destinazione"])
     relP1, relP2 = getRel(id, 1, 0, conn), getRel(id, 2, 0, conn)
     relE1, relE2 = getRel(id, 1, 1, conn), getRel(id, 2, 1, conn)
 
@@ -102,7 +103,7 @@ def relationMatching(id: int, conn: Connection):
         for rel2 in relP2:
             if sameRel(rel1.get("id"), rel2.get("id"), id, 0, conn):
                 if sameTarget(rel1.get("id"), rel2.get("id"), conn):
-                    print("Coincidente [{}] -- [{}]".format(rel1, rel2))  # COINCIDENTI
+                    print("Coincidente [{}] -- [{}] -- sameRel->sameTargetPartenti".format(rel1, rel2))  # COINCIDENTI
                     compl = None
                     toRem.append(rel2.get("id"))
                     break
@@ -128,7 +129,7 @@ def relationMatching(id: int, conn: Connection):
     for rel1 in relE1:
         for rel2 in relE2:
             if areDirectlyContraddictory(rel1.get("tipo"), rel2.get("tipo"), rel1.get("id"), rel2.get("id"), conn):
-                print("Contraddittoria DIRETTA [{}] - [{}]".format(rel1, rel2))  # CONTRADDITTORIA
+                print("Contraddittoria DIRETTA [{}] - [{}] -- areDirContrPartenti".format(rel1, rel2))  # CONTRADDITTORIA
                 toRem.append(rel2.get("id"))
                 # compl = None
                 # break #Per ora tolti
@@ -160,4 +161,5 @@ def relationMatching(id: int, conn: Connection):
             if not b:
                 print("Complementare -->", rel)  # COMPLEMENTARE
 
-    getInfoSemi(id, conn)
+    getInfoSemi(id, conn, df)
+    return df
--- Testing/RelationTest.py
+++ Testing/RelationTest.py
@@ -2,10 +2,31 @@ import sys
 sys.path.insert(0, '..')
 
 from Functions.RelationMatching import relationMatching
+import Functions.Relation as r
+import Functions.Entity as e
 from Functions.Connection import Connection
 
 conn = Connection("bolt+s://e3a58db5.databases.neo4j.io:7687", "neo4j", "KR14Xk19s07EJwooFQ9-3dAnH7cL-b9FxnEAVSmG3f0")
 
-print(relationMatching(1, conn))
+"""
+e.create_instance_mine(conn, "Person", {'Name':'Piero', 'Surname':'Maso'}, 1, 100)
+e.create_instance_mine(conn, "Person", {'Name':'Piero', 'Surname':'Maso'}, 2, 100)
+e.create_instance_mine(conn, "Person", {'Name':'Paola', 'Surname':'Maso'}, 1, 101)
+e.create_instance_mine(conn, "Person", {'Name':'Paola', 'Surname':'Maso'}, 2, 101)
+e.create_instance_mine(conn, "Person", {'Name':'Gina', 'Surname':'Maso'}, 1, 102)
+e.create_instance_mine(conn, "Person", {'Name':'Gina', 'Surname':'Maso'}, 2, 102)
+
+r.create_relation_dir("Person", {'Name':'Paola', 'Surname':'Maso'}, 1, "Person", {'Name':'Piero', 'Surname':'Maso'}, 1, "MADRE_DI", conn)
+r.create_relation_dir("Person", {'Name':'Paola', 'Surname':'Maso'}, 2, "Person", {'Name':'Piero', 'Surname':'Maso'}, 2, "PADRE_DI", conn)
+r.create_relation_dir("Person", {'Name':'Gina', 'Surname':'Maso'}, 2, "Person", {'Name':'Piero', 'Surname':'Maso'}, 2, "MADRE_DI", conn)
+
+e.create_instance_mine(conn, "Person", {'Name':'Sina', 'Surname':'Maso'}, 1, 104)
+e.create_instance_mine(conn, "Person", {'Name':'Sina', 'Surname':'Maso'}, 2, 104)
+r.create_relation_with_attribute("Person", {'Name':'Gina', 'Surname':'Maso'}, 2, "Person", {'Name':'Piero', 'Surname':'Maso'}, 2, "MADRE_DI", {'scadenza':'10-02-2002'}, conn)
+
+print(r.create_relation_dir("Person", {'Name':'Gina', 'Surname':'Maso'}, 2, "Person", {'Name':'Piero', 'Surname':'Maso'}, 2, "PADRE_DI", conn))
+"""
+
+print(relationMatching(100, conn))
 
 conn.close()
